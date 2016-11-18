acbffd5ea591437bc4bceeb7ee7d1241e03fb4b4
uchida <linux@hi-ho.ne.jp>
2016-11-18 20:09:28 +0900

    Commit on Nov/18/2016

--- examples/example_simple/example_simple.js
+++ examples/example_simple/example_simple.js
@@ -1,35 +1,35 @@
 // -----------------------------------------------------------------------
 //	example_simple.js
 //
-//					Aug/20/2013
+//					Nov/12/2016
 //
 // -----------------------------------------------------------------------
 jQuery (function ()
 {
 	jQuery("#outarea_aa").html
-		("*** example_simple *** start *** Aug/20/2013 ***");
+		("*** example_simple *** start *** Nov/12/2016 ***")
 
-	var data_text = "";
+	var data_text = ""
 
-	var file_in = "./in01.txt";
+	var file_in = "./in01.txt"
 
 	jQuery.get (file_in,function (data_text)
 		{
-		var str_out = "<b>source</b></br>";
+		var str_out = "<b>source</b></br>"
 
-		var txt_cc = data_text.replace (/</g,"&lt;");
-		txt_cc = txt_cc.replace (/>/g,"&gt;");
+		var txt_cc = data_text.replace (/</g,"&lt");
+		txt_cc = txt_cc.replace (/>/g,"&gt");
 
-		jQuery("#outarea_ff").html ("<pre>" + txt_cc + "</pre>");
+		jQuery("#outarea_ff").html ("<pre>" + txt_cc + "</pre>")
 
-		str_out += "<pre>" + txt_cc + "</pre>";
+		str_out += "<pre>" + txt_cc + "</pre>"
 
-		jQuery(".contents").html (str_out);
-		});
+		jQuery(".contents").html (str_out)
+		})
 
 	jQuery("#outarea_hh").html
-		("*** example_simple *** end *** Aug/20/2013 ***");
+		("*** example_simple *** end *** Nov/12/2016 ***")
 
-});
+})
 
 // -----------------------------------------------------------------------
