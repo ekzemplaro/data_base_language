0ee0852fbcb6bf5f00570d337430a7ae5a7cf41f
uchida <linux@hi-ho.ne.jp>
2017-01-16 17:50:12 +0900

    Commit on Jan/16/2017

--- doc/web_server/ruby_on_rails/ruby_on_rails.js
+++ doc/web_server/ruby_on_rails/ruby_on_rails.js
@@ -1,34 +1,30 @@
 // -----------------------------------------------------------------------
 //	ruby_on_rails.js
 //
-//					Aug/27/2012
+//					Jan/15/2017
 //
 // -----------------------------------------------------------------------
 jQuery (function ()
 {
-	jQuery("#outarea_aa").text ("*** start *** ruby_on_rails.js ***");
+	show_contents ("install")
+	show_contents ("versions")
+	show_contents ("example")
+	show_contents ("database")
+	show_contents ("passenger")
 
-
-	show_contents ("install");
-	show_contents ("versions");
-	show_contents ("example");
-	show_contents ("database");
-	show_contents ("passenger");
-
-
-	jQuery("#outarea_hh").text ("*** end *** ruby_on_rails.js ***");
-});
+	show_contents ("install_arch")
+})
 
 // -----------------------------------------------------------------------
 function show_contents (contents)
 {
-	var url_txt = contents + ".txt";
-	var place = "#" + contents;
+	var url_txt = contents + ".txt"
+	var place = "#" + contents
 
 	jQuery.get (url_txt,function (data_text)
                 {
-		jQuery (place).html (data_text);
-                });
+		jQuery (place).html (data_text)
+                })
 }
 
 // -----------------------------------------------------------------------
--- doc/web_server/wordpress/go_html_gen.sh
+++
@@ -1,3 +0,0 @@
-DOC=/var/www/data_base/doc
-#
-cat $DOC/head_short.txt head.txt contents.txt $DOC/tail_short.txt > wordpress.html
